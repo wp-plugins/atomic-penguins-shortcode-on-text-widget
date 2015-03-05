a7e5363f6ede9b6b2f15dab1463322323e8ed7c9
hesusko <hesusko@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-05 12:31:07 +0000

    version tag edit
    git-svn-id: https://plugins.svn.wordpress.org/atomic-penguins-shortcode-on-text-widget/tags/1.0@1105987 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mind_blown.php
+++ mind_blown.php
@@ -4,7 +4,7 @@ Plugin Name: Atomic Penguins Shortcode on Text Widget
 Description: Make your text widget accept shortcodes.
 Author: Atomic Penguin
 Author URL: http://atomicpenguins.com/
-Version: 1.1
+Version: 1.0
 */
 
 add_filter('widget_text', 'do_shortcode');
